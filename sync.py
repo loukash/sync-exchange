b2374239691be2a329960635add435d431f536d0
Andrey Lukashin <aglukashin@avito.ru>
2024-02-28 17:18:25 +0500

    new version

--- sync.py
+++ sync.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import pytz
 import caldav
 import getpass
@@ -37,6 +38,10 @@ items = account.calendar.view(
 
 for item in items:
     print(item.subject, item.uid, item.is_all_day, item.start, item.end)
+    # print(item.recurrence_id, item.original_start, item.location, item.when, item.is_meeting, item.is_cancelled, item.is_recurring, item.type, item.recurrence)
+    # print(item.original_start, item.location, item.when, item.is_meeting, item.is_cancelled, item.is_recurring, item.type, item.recurrence)
+    # print(item)
+    # sys.exit()
 
     with caldav.DAVClient(url=caldav_url, username=caldav_user, password=caldav_password, headers=headers) as client:
         my_principal = client.principal()
@@ -46,16 +51,30 @@ for item in items:
         #     print(c.name, c.id, c.url)
 
         my_calendar = my_principal.calendar(cal_id="1-3")  # 1-3 - А. Работа; cal_id, cal_name, cal_url
+        # bad_event = my_calendar.event_by_uid('94458317-9665-4A10-9551-0A584B7B3E74')
+        # print(bad_event)
 
         if item.is_all_day:
             continue
 
-        my_event = my_calendar.save_event(
+        if item.type == 'Exception':
+            continue
+            event = calendar.event_by_uid(item.uid)
+            event.icalendar_component["dtstart"].dt = datetime.datetime.strptime(str(item.start), '%Y-%m-%d %H:%M:%S%z')
+            event.icalendar_component["dtend"].dt = datetime.datetime.strptime(str(item.end), '%Y-%m-%d %H:%M:%S%z')
+            event.save()
+            continue
+
+        try:
+            my_event = my_calendar.save_event(
                 dtstart=datetime.datetime.strptime(str(item.start), '%Y-%m-%d %H:%M:%S%z'),
                 dtend=datetime.datetime.strptime(str(item.end), '%Y-%m-%d %H:%M:%S%z'),
                 uid=item.uid,
                 summary=item.subject,
                 )
+            print(my_event)
+        except Exception as e:
+            print('Exception: {}'.format(str(e)))
+            print('UID: {}'.format(item.uid))
 
-        print(my_event)
 
